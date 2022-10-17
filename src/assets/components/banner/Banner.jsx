896eebd7b82b08a0e691844abee28f389f071e90
Jonathan <jonathanvel84@gmail.com>
2022-10-17 16:48:36 -0500

    commit

--- src/assets/components/banner/Banner.jsx
+++ src/assets/components/banner/Banner.jsx
@@ -1,35 +1,45 @@
-import './Banner.css'
+import "./Banner.css";
 
-import ArrowDown from '../icons/ArrowDown'
+import ArrowDown from "../icons/ArrowDown";
+import VideoUniverse from "../../images/nebula_red_-_31420 (Original).mp4";
 
-function Banner (){
-    return(
-        <>
-        <header>
-            <ul>
-                <li><a href="#image-day">Image of the Day</a></li>
-                <li><a href="#explore">Explore</a></li>
-            </ul>
-        </header>
-        <section className='container-banner'>
-            <div className='video-banner'>
-                <video src="src/assets/images/nebula_red_-_31420 (Original).mp4"
-                muted
-                autoPlay
-                loop
-                type="video/mp4"
-                ></video>
-            </div>
-            <div className='banner-text'>
-                <h1>EXPLORE THE UNIVERSE</h1>
-                <ul>
-                    <li><a href="#image-day">Scroll</a></li>
-                    <li><ArrowDown /></li>
-                </ul>
-            </div>
-        </section>
-        </>
-    );
+function Banner() {
+  return (
+    <>
+      <header>
+        <ul>
+          <li>
+            <a href="#image-day">Image of the Day</a>
+          </li>
+          <li>
+            <a href="#explore">Explore</a>
+          </li>
+        </ul>
+      </header>
+      <section className="container-banner">
+        <div className="video-banner">
+          <video
+            src={VideoUniverse}
+            muted
+            autoPlay
+            loop
+            type="video/mp4"
+          ></video>
+        </div>
+        <div className="banner-text">
+          <h1>EXPLORE THE UNIVERSE</h1>
+          <ul>
+            <li>
+              <a href="#image-day">Scroll</a>
+            </li>
+            <li>
+              <ArrowDown />
+            </li>
+          </ul>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default Banner
+export default Banner;
